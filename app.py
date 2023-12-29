2b8b0b8327e68b11b7d1f49c93441004c2268ad9
Mark Lisi <marklisi951@gmail.com>
2023-12-29 16:50:07 -0500

    `bets` route loads in NBA games/odds

--- app.py
+++ app.py
@@ -12,16 +12,26 @@ def home():
 @app.route('/bets')
 def bets():
     odds_response = requests.get(
-    f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
+    'https://api.the-odds-api.com/v4/sports/basketball_nba/odds',
     params={
-        'api_key': os.getenv('API_KEY'),
+        'api_key': '9bbb3bda82ce604880f9160ed6b6497c',
         'regions': 'us',
-        'markets': 'h2h,spreads',
+        'markets': 'h2h',
         'oddsFormat': 'decimal',
         'dateFormat': 'iso',
-    }
-)
-    return render_template('bets.html', odds_response=odds_response)
+        'bookmakers': 'draftkings'
+    })
+
+    games = []
+    for game in odds_response.json():
+        home_team = game['bookmakers'][0]['markets'][0]['outcomes'][0]['name']
+        home_odds = game['bookmakers'][0]['markets'][0]['outcomes'][0]['price']
+        away_team = game['bookmakers'][0]['markets'][0]['outcomes'][1]['name']
+        away_odds = game['bookmakers'][0]['markets'][0]['outcomes'][1]['price']
+        games.append(f'{home_team} ({home_odds}) versus {away_team} ({away_odds})')
+
+
+    return render_template('bets.html', games=games)
 
 if __name__ == '__main__':
     app.run(debug=True)
